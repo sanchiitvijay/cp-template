bf0c914a99b2236f95bc6c34d7c1d12b673347ab
sanchiitvijay <sanchiitvijay@gmail.com>
2025-02-17 01:41:24 +0530

    updated

--- template.cpp
+++ template.cpp
@@ -1,6 +1,29 @@
 #include <bits/stdc++.h>
-using namespace std;
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp> 
 
+
+// using namespace __gnu_pbds;
+// typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
+
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define forinc(i,a,b) for (i = a; i < b; i++)
+#define fordec(i,a,b) for (i = a; i >= b; i--)
+#define pii pair<int, int>
+
+
+void display(vector<int> v){
+    for (auto i : v) cout << i << ' ';
+    cout << "\n";
+}
 
 
 struct DSU {
@@ -90,3 +113,32 @@ void creatingSubset(int n) {
         }
     }
 }
+
+
+
+void solve() {
+    // code here
+    
+
+    return;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+    ll t = 1;
+    cin >> t;
+
+    while (t--){
+        // cout<<"-----------"<<endl;
+        solve();
+        cout << "\n";
+    }
+
+    return 0;
+}
+
+// max heap
+// priority_queue<int,vector<int>,greater<int>> pq;
